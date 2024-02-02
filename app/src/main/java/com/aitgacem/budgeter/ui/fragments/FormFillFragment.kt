4aa942351197767230bd8383641077633ecb3953
Nabil <aitgacema@gmail.com>
2024-02-02 15:55:15 +0100

    enhanced form filling screen

--- app/src/main/java/com/aitgacem/budgeter/ui/fragments/FormFillFragment.kt
+++ app/src/main/java/com/aitgacem/budgeter/ui/fragments/FormFillFragment.kt
@@ -6,12 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.aitgacem.budgeter.databinding.FragmentFormfillBinding
-import com.aitgacem.budgeter.ui.viewmodels.AnalyticsViewModel
+import com.aitgacem.budgeter.ui.viewmodels.DepositViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.Calendar
 
+@AndroidEntryPoint
 class FormFillFragment : Fragment() {
     lateinit var binding: FragmentFormfillBinding
-    private val viewModel: AnalyticsViewModel by viewModels()
+    private val viewModel: DepositViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,5 +32,32 @@ class FormFillFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.saveBtn.setOnClickListener {
+            viewModel.updateDescription(binding.transactionTitle.editText?.text.toString())
+            viewModel.updateAmount(binding.transactionAmount.editText?.text.toString())
+            val (day, month, year) = binding.transactionDate.let {
+                Triple(it.dayOfMonth, it.month, it.year)
+            }
+            val timestamp = getTimestamp(day, month, year)
+            viewModel.updateDate(timestamp)
+            viewModel.saveTransaction()
+            findNavController().popBackStack()
+        }
     }
+}
+
+private fun getTimestamp(day: Int, month: Int, year: Int): Long {
+    val calendar = Calendar.getInstance()
+    calendar.set(Calendar.DAY_OF_MONTH, day)
+    calendar.set(Calendar.MONTH, month - 1)  // Calendar months are zero-based
+    calendar.set(Calendar.YEAR, year)
+
+    // Set the time to midnight (00:00:00)
+    calendar.set(Calendar.HOUR_OF_DAY, 0)
+    calendar.set(Calendar.MINUTE, 0)
+    calendar.set(Calendar.SECOND, 0)
+    calendar.set(Calendar.MILLISECOND, 0)
+
+    return calendar.timeInMillis
 }
