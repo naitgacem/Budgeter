6601080b8aaeb0acf196619647f82aeba4cdb7da
Nabil <aitgacema@gmail.com>
2024-02-02 09:39:46 +0100

    added bottom navigation

--- app/src/main/java/com/aitgacem/budgeter/MainActivity.kt
+++ app/src/main/java/com/aitgacem/budgeter/MainActivity.kt
@@ -3,7 +3,9 @@ package com.aitgacem.budgeter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
-import com.aitgacem.budgeter.ui.fragments.OverviewFragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import java.lang.ref.WeakReference
 
@@ -13,17 +15,16 @@ object CurrentActivityHolder {
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            val fragment = OverviewFragment()
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.main_activity_host, fragment)
-                .commit()
-        }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_activity_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
+
     }
 
 
