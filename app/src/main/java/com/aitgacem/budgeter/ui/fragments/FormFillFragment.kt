0321ea9d2c290a701fa6eb9968bde5e317c9b3d1
Nabil <aitgacema@gmail.com>
2024-02-05 11:42:48 +0100

    enhancement

--- app/src/main/java/com/aitgacem/budgeter/ui/fragments/FormFillFragment.kt
+++ app/src/main/java/com/aitgacem/budgeter/ui/fragments/FormFillFragment.kt
@@ -1,29 +1,34 @@
 package com.aitgacem.budgeter.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.aitgacem.budgeter.R
 import com.aitgacem.budgeter.databinding.FragmentFormfillBinding
 import com.aitgacem.budgeter.ui.components.Category
+import com.aitgacem.budgeter.ui.components.toIcon
 import com.aitgacem.budgeter.ui.viewmodels.FormFillViewModel
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipDrawable
 import com.google.android.material.transition.MaterialSharedAxis
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Calendar
 
 @AndroidEntryPoint
 class FormFillFragment : Fragment() {
-    lateinit var binding: FragmentFormfillBinding
+
+    private lateinit var binding: FragmentFormfillBinding
     private val viewModel: FormFillViewModel by viewModels()
     private var isDeposit: Boolean = false
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,11 +37,30 @@ class FormFillFragment : Fragment() {
     ): View {
         binding = FragmentFormfillBinding.inflate(layoutInflater, container, false)
         val args: FormFillFragmentArgs by navArgs()
+
         isDeposit = args.isDeposit
+        val context = requireContext()
+
+        setupSelectionChips(context, isDeposit)
+        return binding.root
+    }
+
+    private fun setupSelectionChips(context: Context, isDeposit: Boolean) {
         if (isDeposit) {
             binding.transactionCategory.visibility = GONE
+        } else {
+            Category.entries.forEach {
+                if (it != Category.Deposit) {
+                    val chip = Chip(context)
+                    val drawable =
+                        ChipDrawable.createFromAttributes(context, null, 0, R.style.CustomChipStyle)
+                    chip.setChipDrawable(drawable)
+                    chip.text = it.name
+                    chip.chipIcon = AppCompatResources.getDrawable(context, it.toIcon())
+                    binding.transactionCategory.addView(chip)
+                }
+            }
         }
-        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
