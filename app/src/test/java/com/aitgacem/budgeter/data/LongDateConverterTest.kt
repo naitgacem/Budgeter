d9833783e766a432cab3a57db651038a3692669f
Nabil <aitgacema@gmail.com>
2024-02-06 13:02:37 +0100

    added a test

--- app/src/test/java/com/aitgacem/budgeter/data/LongDateConverterTest.kt
+++ app/src/test/java/com/aitgacem/budgeter/data/LongDateConverterTest.kt
@@ -1,22 +1,28 @@
 package com.aitgacem.budgeter.data
 
+import com.aitgacem.budgeter.ui.getDayMonthYearFromTimestamp
+import com.aitgacem.budgeter.ui.getTimestamp
 import org.junit.Test
 import com.google.common.truth.Truth.assertThat
 import java.text.SimpleDateFormat
 import java.util.Date
+import kotlin.math.abs
+import kotlin.random.Random
 
 class LongDateConverterTest {
     @Test
-    fun `test date formatting`() {
-        val timestamp = "1707001200000"
-        val dateStr = "04/02/2024"
-        assertThat(longToDateStr(timestamp)).isEqualTo(dateStr)
+    fun testDateFormatting() {
+        for (i in 1..100) {
+            val timestamp = abs(Random.Default.nextLong())
+            val date: Triple<Int, Int, Int> = getDayMonthYearFromTimestamp(timestamp)
+            assertThat(
+                getTimestamp(
+                    date.first,
+                    date.second,
+                    date.third
+                )
+            ).isWithin(24 * 3600 * 1000L).of(timestamp)
+        }
     }
 
-    private fun longToDateStr(timestamp: String): String {
-        val date: Long = timestamp.toLong()
-        val dateFormat = SimpleDateFormat("dd/MM/yyyy")
-        val dateObj = Date(date)
-        return dateFormat.format(dateObj)
-    }
 }
