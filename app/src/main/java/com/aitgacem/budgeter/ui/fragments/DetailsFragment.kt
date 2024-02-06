386fcaeab0bf19de8bea3d512c2e59575518fde6
Nabil <aitgacema@gmail.com>
2024-02-06 17:39:58 +0100

    removed unused code

--- app/src/main/java/com/aitgacem/budgeter/ui/fragments/DetailsFragment.kt
+++ app/src/main/java/com/aitgacem/budgeter/ui/fragments/DetailsFragment.kt
@@ -2,27 +2,19 @@ package com.aitgacem.budgeter.ui.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.content.res.AppCompatResources
-import androidx.core.view.MenuHost
-import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
-import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.navigation.navGraphViewModels
 import com.aitgacem.budgeter.R
 import com.aitgacem.budgeter.databinding.FragmentDetailsBinding
 import com.aitgacem.budgeter.ui.components.Category
 import com.aitgacem.budgeter.ui.components.ItemType
-import com.aitgacem.budgeter.ui.toFormattedDate
 import com.aitgacem.budgeter.ui.components.toIcon
+import com.aitgacem.budgeter.ui.toFormattedDate
 import com.aitgacem.budgeter.ui.viewmodels.FormFillViewModel
 
 class DetailsFragment : Fragment() {
@@ -97,17 +89,7 @@ class DetailsFragment : Fragment() {
                 }
             }
         }
-        val menuHost = requireActivity() as MenuHost
-        menuHost.addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menuInflater.inflate(R.menu.menu_transaction_details, menu)
-            }
 
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                return true
-            }
-
-        })
 
     }
 }
