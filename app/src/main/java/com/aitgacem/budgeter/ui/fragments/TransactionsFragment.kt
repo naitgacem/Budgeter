084766c4be447a6d59fe5d0718471c1e5b21615f
AIT GACEM Nabil <aitgacema@gmail.com>
2024-02-23 17:47:13 +0100

    added saerch

--- app/src/main/java/com/aitgacem/budgeter/ui/fragments/TransactionsFragment.kt
+++ app/src/main/java/com/aitgacem/budgeter/ui/fragments/TransactionsFragment.kt
@@ -8,21 +8,19 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.aitgacem.budgeter.data.TransactionsRepository
 import com.aitgacem.budgeter.databinding.FragmentTransactionsBinding
 import com.aitgacem.budgeter.ui.components.DayDecorator
-import com.aitgacem.budgeter.ui.components.ItemType
 import com.aitgacem.budgeter.ui.components.HistoryRvAdapter
+import com.aitgacem.budgeter.ui.components.ItemType
 import com.aitgacem.budgeter.ui.viewmodels.TransactionsViewModel
+import com.google.android.material.search.SearchView
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class TransactionsFragment : Fragment() {
 
     private lateinit var binding: FragmentTransactionsBinding
     private val viewModel: TransactionsViewModel by viewModels()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,13 +49,23 @@ class TransactionsFragment : Fragment() {
         val dividerItemDecoration = DayDecorator()
         recyclerView.addItemDecoration(dividerItemDecoration)
 
-        viewModel.transactions.observe(viewLifecycleOwner) { it ->
+        viewModel.filteredList.observe(viewLifecycleOwner) { it ->
             val new = mutableListOf<ItemType>()
             for ((key, value) in it) {
-                new.addAll(mutableListOf(key))
-                new.addAll(value)
+                if (value.isNotEmpty()) {
+                    new.addAll(mutableListOf(key))
+                    new.addAll(value)
+                }
             }
             listAdapter.submitList(new)
+        }
+
+        val searchView: SearchView = binding.openSearchView
+        searchView.editText.setOnEditorActionListener { v, actionId, event ->
+            binding.openSearchBar.setText(searchView.text)
+            searchView.hide()
+            viewModel.updateFilter(searchView.text.toString())
+            false
         }
     }
 }
--- app/src/main/java/com/aitgacem/budgeter/ui/viewmodels/TransactionsViewModel.kt
+++ app/src/main/java/com/aitgacem/budgeter/ui/viewmodels/TransactionsViewModel.kt
@@ -1,7 +1,12 @@
 package com.aitgacem.budgeter.ui.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+import androidx.lifecycle.switchMap
 import com.aitgacem.budgeter.data.TransactionsRepository
+import com.aitgacem.budgeter.ui.components.ItemType
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -9,5 +14,22 @@ import javax.inject.Inject
 class TransactionsViewModel @Inject constructor(
     private val repository: TransactionsRepository,
 ) : ViewModel() {
-    val transactions = repository.getDayAndTransactions()
+    private var _filter = MutableLiveData("")
+
+    val transactions: LiveData<Map<ItemType.Date, List<ItemType.Transaction>>> =
+        repository.getDayAndTransactions()
+
+    val filteredList = _filter.switchMap { filter ->
+        repository.getDayAndTransactions().map { map ->
+            map.mapValues { (_, list) ->
+                list.filter {
+                    it.title.contains(filter.toString())
+                }
+            }
+        }
+    }
+
+    fun updateFilter(filter: String) {
+        _filter.value = filter
+    }
 }
